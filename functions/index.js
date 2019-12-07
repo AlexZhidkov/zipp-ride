80f9efd3f069d474b92c1d53e60efc5021724aaf
Alexander Zhidkov <azhidkov@gmail.com>
2019-12-07 16:04:10 +0800

    send event email

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const emailReceiver = functions.config().email.receiver;
+
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
+});
+
+// Your company name to include in the emails
+const APP_NAME = 'ZIPP Ride';
+
+/**
+ * Sends an email for each new document in firestore collection.
+ */
+exports.sendEmail = functions.firestore
+    .document('events/{emailId}')
+    .onCreate((snap, context) => {
+        return sendNotificationEmail(snap.data());
+    });
+
+// Sends a notification email
+async function sendNotificationEmail(event) {
+    const mailOptions = {
+        from: `${APP_NAME}`,
+        to: emailReceiver,
+    };
+
+    mailOptions.subject = event.event;
+    mailOptions.text = JSON.stringify(event.data);
+
+    await mailTransport.sendMail(mailOptions);
+    console.log('Notification email sent to:', emailReceiver);
+    return null;
+}
+
